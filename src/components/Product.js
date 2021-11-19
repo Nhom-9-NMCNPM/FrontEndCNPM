fda39a51bc42f23a144c6a581c342f85daddc2c7
manhdat <tangdatsdbg@gmail.com>
2021-11-19 14:12:29 +0700

    Update FrontEnd New

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,8 @@ import Product4 from "../components/Product/Product4"
 import NavHeader from "../components/HomePage/NavHeader"
 import Footer from "./HomePage/Footer"
 import FindShop from "./HomePage/FindShop"
-const Product=() => {
+import { connect } from "react-redux"
+const Product=({dress,trousers}) => {
     return (
        <div>
            <NavHeader />
@@ -14,9 +15,9 @@ const Product=() => {
 
                 <div class="collection-body container">
                     <Product2Left />
-                    <Product4 value={false} />
+                    <Product4  data={dress}/>
                     <Product2Right />
-                    <Product4 value={true} />
+                    <Product4 data={trousers} />
                 </div>
             </div>
             <FindShop />
@@ -24,5 +25,11 @@ const Product=() => {
        </div>
     )
 }
+const mapStateToProps = (state)  => {
+    return {
+        dress: state.Dress,
+        trousers: state.Trousers,
 
-export default Product
+    }
+}
+export default connect(mapStateToProps)(Product)
--- src/components/Product/Product4.js
+++ src/components/Product/Product4.js
@@ -1,40 +1,11 @@
 import '../../style/Product/Product.css'
-import {connect} from 'react-redux'
 import format_curency from '../../utils/displayPrice'
 import { Link } from 'react-router-dom'
-const Product4 = ({dress ,trousers}, check ) => {
-    return (check.value) ?(
+const Product4 = ({data} ) => {
+    return (
             <div>
                 <div className="product-list row">
-                        {dress.map((item,index) => {
-                            return (index <= 3)  && (
-                                <div className="product col-3" key={index}> 
-                                    <Link to="/">
-                                        <div className="product-img">
-                                            <img className="img-change" src={item.img[0]} alt=""/>
-                                            <img className="img-after-change" src={item.img[1]} alt=""/>       
-                                            {item.id % 2 === 0 && <img className="new-arrivals" src="https://file.hstatic.net/200000000133/file/1_113da73eef75425786591a547aeda483.png" alt=""/>}
-                                            <img className="best-seller" src="https://file.hstatic.net/200000000133/file/labels-final_b1d9901b63944df28810efb035e03fcb.png" alt=""/>
-                                        </div>
-                                    </Link>
-                                    <div className="product-detail">
-                                        <Link to="/">{item.name}</Link>
-                                        <div>
-                                            <span style={{fontSize: "13px"}}>{format_curency(item.price)}đ</span>
-                                        </div>
-                                    </div>      
-                                </div>
-                            )
-                        })}
-                        
-                </div>
-            </div>
-        )
-    :
-         (
-            <div>
-                <div className="product-list row">
-                        {trousers.map((item,index) => {
+                        {data.map((item,index) => {
                             return (index <= 3)  && (
                                 <div className="product col-3" key={index}> 
                                     <Link to="/">
@@ -61,11 +32,5 @@ const Product4 = ({dress ,trousers}, check ) => {
     }
  
 
-const mapStateToProps = (state)  => {
-    return {
-        dress: state.Dress,
-        trousers: state.Trousers,
 
-    }
-}
-export default connect(mapStateToProps)(Product4)
+export default Product4
